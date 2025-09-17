b8b5880a5fd224b3b57605bf7986ae17a55bfa5a
JangAyeon <mediarkayeon@gmail.com>
2025-09-17 14:11:37 +0900

    add nest instance modules+controller

--- todokeep-api/src/app.module.ts
+++ todokeep-api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { BoardsModule } from './boards/boards.module';
+import { UploadModule } from './upload/upload.module';
+import { SocketModule } from './socket/socket.module';
+import { EventsGateway } from './events/events.gateway';
 
 @Module({
-  imports: [],
+  imports: [TasksModule, BoardsModule, UploadModule, SocketModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EventsGateway],
 })
 export class AppModule {}
