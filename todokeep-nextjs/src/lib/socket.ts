d64124ce0a7a2399570146021c2a0ce33c760b53
JangAyeon <ayeon3052@ewhain.net>
2025-09-22 23:52:26 +0900

    feat: set to use env

--- todokeep-nextjs/src/lib/socket.ts
+++ todokeep-nextjs/src/lib/socket.ts
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3000", {
+
+console.log("SOCKET_URL", process.env.NEXT_PUBLIC_SOCKET_URL);
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
   path: "/socket.io", // default, change only if backend uses different path
   reconnection: true,
   reconnectionAttempts: Infinity,
   randomizationFactor: 0.5,
+  transports: ["websocket", "polling"], // polling 추가 (중요!)
 });
 
 export default socket;
